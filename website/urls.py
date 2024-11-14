66606fbd18939608b7c5aa31ab5f4b8a3007e3ba
NickEscc <150950750+NickEscc@users.noreply.github.com>
2024-11-14 12:32:36 -0600

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
-    path('HowToPlay/', views.HowToPlay, name='how_to_play'), #How To Play page
+    path('HowToPlay/', views.how_to_play, name='how_to_play'), #How To Play page
 ]
