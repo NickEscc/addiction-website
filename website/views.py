b3c345e78cc9071f4167024418739d680aad0c22
NickEscc <150950750+NickEscc@users.noreply.github.com>
2024-11-14 12:18:20 -0600

    Updated HowToPlaly

--- website/views.py
+++ website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'website/home.html')
+
+def HowToPlay(request):
+    return render(request, 'website/HowToPlay.html')
