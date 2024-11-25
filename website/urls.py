d720bc1b6a485bd12ee92bddf49348f66bbfd2b4
Bryan Barrios <Bryanbarrios@gmail.com>
2024-11-25 17:32:02 -0600

    Theres no code in website/HowItWork.html. Home html is requesting  text css and theres no code in it

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('HowToPlay/', views.HowToPlay, name='HowToPlay'), #How To Play page
-    path('HowItWorks/', views.HowItWorks, name='HowItWorks'), #How It Works page
+    # path('HowItWorks/', views.HowItWorks, name='HowItWorks'), #How It Works page 
 ]
