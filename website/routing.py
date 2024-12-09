531022453a1ce493aa3ce0d2b885163b3a79ab56
isthathuddy <huddyw.20@gmail.com>
2024-12-08 23:02:34 -0600

    new working vers of poker game

--- website/routing.py
+++
@@ -1,8 +0,0 @@
-# website/routing.py
-
-from django.urls import re_path
-from .consumer import PokerGameConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/Services/(?P<connection_channel>[^/]+)/$', PokerGameConsumer.as_asgi()),
-]
