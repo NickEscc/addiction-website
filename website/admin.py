ec5d0db1301a053286ecf1e6d0c6c8ce0da42c0d
Bryan Barrios <Bryanbarrios@gmail.com>
2024-12-10 22:14:03 -0600

    added DATAbase

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,5 @@
+# website/admin.py
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+admin.site.register(UserProfile)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,21 @@
+# website/models.py
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=1000.00)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        # Create a new UserProfile for newly created user
+        UserProfile.objects.create(user=instance)
+    else:
+        # For existing users, get_or_create the profile
+        UserProfile.objects.get_or_create(user=instance)
